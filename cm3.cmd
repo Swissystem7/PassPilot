6f417cace0e679b3c4ee926cfc085c0076b9fe99
Swissystem7 <aviran2606@gmail.com>
2026-07-29 18:10:23 +0300

    chore: remove scratch build files

--- cm3.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d D:\claude-node\jobs\repos\PassPilot
-set GIT="C:\Program Files\Git\cmd\git.exe"
-%GIT% add -A
-%GIT% -c user.name=Swissystem7 -c user.email=aviran2606@gmail.com commit -q -F msg3.txt
-%GIT% push -q origin HEAD
-echo PUSHED
